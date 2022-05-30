61ba8d0fd11299bc2b6116c76fcd4017ccae05f6
damisettimo <damisettimo@gmail.com>
2022-05-29 22:44:47 -0300

    agregados

--- app_coder/admin.py
+++ app_coder/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
+<<<<<<< HEAD
 from app_coder.models import Categoria, Student, Profesor, Homework
+=======
+from app_coder.models import Course, Entrenador, Student, Homework
+>>>>>>> pr/2
 
 admin.site.register(Categoria)
 
 admin.site.register(Student)
 
-admin.site.register(Profesor)
+admin.site.register(Entrenador)
 
 admin.site.register(Homework)
--- app_coder/forms.py
+++ app_coder/forms.py
@@ -1,15 +1,19 @@
 import datetime
 from django import forms
-from django.forms import ModelForm
-from app_coder.models import Profesor
+# from django.forms import ModelForm
 
 
 class CategoriaeForm(forms.Form):
     name = forms.CharField(max_length=40, min_length=3, label='Nombre')
     code = forms.IntegerField(label='Camada')
 
+<<<<<<< HEAD
 #d
 class ProfesorForm(forms.Form):
+=======
+
+class EntrenadorForm(forms.Form):
+>>>>>>> pr/2
     name = forms.CharField(max_length=40, min_length=3, label='Nombre')
     last_name = forms.CharField(max_length=40, label='Apellido')
     email = forms.EmailField(label='Correo electrónico')
