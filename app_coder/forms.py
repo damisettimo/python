e1c0e77dbb7898dbb79f36f9b1cb3aac7d0d5997
Cristian Ance <cristianndan14@gmail.com>
2022-05-30 21:56:08 -0300

    cambios en errores de sintaxis

--- app_coder/forms.py
+++ app_coder/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.forms import ModelForm
 
 
-class CategoriaeForm(forms.Form):
+class CategoriaForm(forms.Form):
     name = forms.CharField(max_length=40, min_length=3, label='Nombre')
     code = forms.IntegerField(label='Camada')
 
--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,37 +1,37 @@
 from django.urls import path
-
 from app_coder import views
+
 
 app_name='app_coder'
 urlpatterns = [
     path('', views.index, name='Home'),
     path('entrenadores', views.entrenadores, name='Entrenadores'),
-    path('categorias', views.courses, name='categoria-list'),
-    path('jugadores', views.students, name='Jugadores'),
+    path('categorias', views.categoriass, name='categoria-list'),
+    path('jugadores', views.jugadores, name='Jugadores'),
     path('homeworks', views.homeworks, name='Homeworks'),
     path('formHTML', views.form_hmtl),
-    path('course-django-forms', views.course_forms_django, name='CourseDjangoForms'),
+    path('course-django-forms', views.categoria_forms_django, name='CourseDjangoForms'),
     path('entrenador-django-forms', views.entrenador_forms_django, name='EntrenadorDjangoForms'),
     path('entrenador/<int:pk>/update', views.update_entrenador, name='UpdateEntrenador'),
     path('entrenador/<int:pk>/delete', views.delete_entrenador, name='DeleteEntrenador'),
     path('homework-django-forms', views.homework_forms_django, name='HomeworkDjangoForms'),
     path('search', views.search, name='Search'),
+    ]
 
-
-    Dajngo documentation -->  https://docs.djangoproject.com/en/4.0/topics/class-based-views/generic-editing/
-    Confirmo la url de la documentación es correcta, deben hacer scroll hasta esta parte:
+    # Dajngo documentation -->  https://docs.djangoproject.com/en/4.0/topics/class-based-views/generic-editing/
+    # Confirmo la url de la documentación es correcta, deben hacer scroll hasta esta parte:
     #
-    from django.urls import path
-    from myapp.views import AuthorCreateView, AuthorDeleteView, AuthorUpdateView
+from django.urls import path
+from app_coder.views import * #AuthorCreateView, AuthorDeleteView, AuthorUpdateView
 
-    # urlpatterns = [
+    
     #     # ...
     #     path('author/add/', AuthorCreateView.as_view(), name='author-add'),
     #     path('author/<int:pk>/', AuthorUpdateView.as_view(), name='author-update'),
     #     path('author/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author-delete'),
     # ]
     
-
+urlpatterns = [
     path('categorias', views.CategoriaListView.as_view(), name='categoria-list'),
     path('categoria/add/', views.CategoriaCreateView.as_view(), name='categoria-add'),
     path('categoria/<int:pk>/detail', views.CategoriaDetailView.as_view(), name='categoria-detail'),
